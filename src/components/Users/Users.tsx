b065508086af474809b4a1549af4f507f80086b6
kwin11bvb@yandex.by <kwin11bvb@yandex.by>
2021-03-18 18:25:45 +0300

    life cycle check

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -10,55 +10,54 @@ type PropsType = {
     setUsers: (users: Array<OneUserData>) => void
 }
 
-const Users: React.FC<PropsType> = (props) => {
+class Users extends React.Component<PropsType> {
 
-    const getUsers = () => {
-        if (props.userData.length === 0) {
-            axios.get('https://social-network.samuraijs.com/api/1.0/users').then((response: any) => {
-                props.setUsers(response.data.items)
-            })
-        }
+    componentDidMount(): void {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then((response: any) => {
+            this.props.setUsers(response.data.items)
+        })
     }
+    
+    render(): React.ReactNode {
+        return (
+            <div className={s.container}>
+                {this.props.userData.map(u => <div key={u.id}>
+                    <div className={s.oneUser}>
+                        <div className={s.areaUser}>
+                            <div>
+                                <img className={s.avatar} src={u.photos.small !== null ? u.photos.small : ''}/>
+                            </div>
+                            <div>
+                                {u.followed
+                                    ? <button className={s.button}
+                                              onClick={() => {
+                                                  this.props.unFollow(u.id)
+                                              }}
+                                    >Unfollow</button>
+                                    : <button className={s.button}
+                                              onClick={() => {
+                                                  this.props.follow(u.id)
+                                              }}
+                                    >Follow</button>}
+                            </div>
+                        </div>
+                    </div>
 
-    return (
-        <div className={s.container}>
-            <button onClick={getUsers}></button>
-            {props.userData.map(u => <div key={u.id}>
-                <div className={s.oneUser}>
-                    <div className={s.areaUser}>
+                    <div className={s.descriptionArea}>
                         <div>
-                            <img className={s.avatar} src={u.photos.small !== null ? u.photos.small : ''}/>
+                            <div>{u.name}</div>
+                            <div>{u.status}</div>
                         </div>
                         <div>
-                            {u.followed
-                                ? <button className={s.button}
-                                          onClick={() => {
-                                              props.unFollow(u.id)
-                                          }}
-                                >Unfollow</button>
-                                : <button className={s.button}
-                                          onClick={() => {
-                                              props.follow(u.id)
-                                          }}
-                                >Follow</button>}
+                            <div>{'u.location.country'}</div>
+                            <div>{'u.location.city'}</div>
                         </div>
                     </div>
-                </div>
-
-                <div className={s.descriptionArea}>
-                    <div>
-                        <div>{u.name}</div>
-                        <div>{u.status}</div>
-                    </div>
-                    <div>
-                        <div>{'u.location.country'}</div>
-                        <div>{'u.location.city'}</div>
-                    </div>
-                </div>
-            </div>)
-            }
-        </div>
-    );
+                </div>)
+                }
+            </div>
+        );
+    }
 }
 
 export default Users;
