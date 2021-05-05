d0ec05ded76a01ad63009077b42445dec886f2eb
kwin64 <kwin11bvb@yandex.by>
2021-05-05 23:36:37 +0300

    add in component Login Form and install redux-form

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,6 +1,32 @@
 import React from "react";
 
-const Login = () => {
-    return <h1>LOGIN</h1>
+const LoginForm = () => {
+    return (
+        <form>
+            <div>
+                <input placeholder={'Login'}/>
+            </div>
+            <div>
+                <input placeholder={'Password'}/>
+            </div>
+            <div>
+                <input type={'checkbox'}/> remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    )
 }
+
+
+const Login = () => {
+    return (
+        <div>
+            <h1>Login</h1>
+            <LoginForm/>
+        </div>
+    )
+}
+
 export default Login;
