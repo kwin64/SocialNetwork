be80d151d6352eb20a67adeb2dd0db44e54c6a12
kwin64 <kwin11bvb@yandex.by>
2021-05-05 15:12:05 +0300

    change status handler

--- src/components/Profile/MyPosts/Posts/ProfileStatus.tsx
+++ src/components/Profile/MyPosts/Posts/ProfileStatus.tsx
@@ -10,19 +10,33 @@ class ProfileStatus extends React.Component<PropsType> {
     state = {
         editMode: false
     }
+    activeEditMode = () => {
+        console.log()
+        this.setState({
+            editMode: true
+        })
+    }
+    deactiveEditMode = () => {
+        console.log()
+        this.setState({
+            editMode: false
+        })
+    }
 
     render() {
         return (
             <div>
                 {!this.state.editMode &&
-                    <div>
-                        <span>{this.props.status}</span>
-                    </div>
+                <div>
+                    <span onDoubleClick={this.activeEditMode}>{this.props.status}</span>
+                </div>
                 }
                 {this.state.editMode &&
-                    <div>
-                        <input value={this.props.status}/>
-                    </div>
+                <div>
+                    <input onBlur={this.deactiveEditMode.bind(this)}
+                           autoFocus={true}
+                           value={this.props.status}/>
+                </div>
                 }
             </div>
         );
