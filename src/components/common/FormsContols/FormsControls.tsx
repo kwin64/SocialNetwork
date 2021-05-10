b687839df7aa6ce28f0fc6bea52fcee6109705c6
kwin64 <kwin11bvb@yandex.by>
2021-05-10 23:31:03 +0300

    refactoring duplicate code for FormControls

--- src/components/common/FormsContols/FormsControls.tsx
+++ src/components/common/FormsContols/FormsControls.tsx
@@ -4,12 +4,12 @@ import s from './FormsControls.module.css'
 type PropsType = {}
 
 // @ts-ignore
-export const Textarea: React.FC<PropsType> = ({input, meta, ...props}) => {
+const FormControl: React.FC<PropsType> = ({input, meta, child, ...props}) => {
     const hasError = meta.touched && meta.error
     return (
         <div className={s.formControl + '' + (hasError ? s.error : '')}>
             <div>
-                <textarea {...input}{...props}/>
+                {props.children}
             </div>
             {hasError && <span>{meta.error}</span>}
         </div>
@@ -17,15 +17,15 @@ export const Textarea: React.FC<PropsType> = ({input, meta, ...props}) => {
 }
 
 // @ts-ignore
+export const Textarea: React.FC<PropsType> = (props) => {
+    // @ts-ignore
+    const {input, meta, child, ...restProps} = props
+    return <FormControl {...props}><textarea {...input} {...restProps}/></FormControl>
+}
 
-export const Input: React.FC<PropsType> = ({input, meta, ...props}) => {
-    const hasError = meta.touched && meta.error
-    return (
-        <div className={s.formControl + '' + (hasError ? s.error : '')}>
-            <div>
-                <input {...input}{...props}/>
-            </div>
-            {hasError && <span>{meta.error}</span>}
-        </div>
-    )
+// @ts-ignore
+export const Input: React.FC<PropsType> = (props) => {
+    // @ts-ignore
+    const {input, meta, child, ...restProps} = props
+    return <FormControl><input {...input}{...restProps}/></FormControl>
 }
